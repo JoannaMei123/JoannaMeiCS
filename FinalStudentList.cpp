718d5103bd611ea6fbc35504ca4fd1b2659f3e35
U-BSD\418556 <418556@SHS-94W2NW3.beaverton.k12.or.us>
2024-11-12 12:08:33 -0800

    Final version!

---
+++ FinalStudentList.cpp
@@ -0,0 +1,142 @@
+
+#include <iostream>
+#include <ctime>
+#include <cstring>
+#include <bits/stdc++.h>
+#include <vector>
+#include <iterator>
+
+/*
+
+  Student List
+  11/5/24
+  Joanna Mei
+
+  You are tasked to create a student list. You can either add a student, print all the students. delete\
+ students or quit trying. Below are the instructions for each command, and what they do.
+
+
+
+
+ */
+
+
+using namespace std;
+
+//Creating the struct that stores all the information of the student
+struct structureInfo {
+  char firstName[100];
+  char lastName[100];
+  int studentId;
+  float GPA;
+};
+
+void addCmd(vector <structureInfo>&vecList);
+void printCmd(vector <structureInfo>&vecList);
+void deleteCmd(vector <structureInfo>&vecList);
+
+int main() {
+  //Initiating vector
+  vector<structureInfo> vecList;
+
+
+
+  while (true) {
+
+  cout << "Please type a command in the prompt below. Type ADD to create a new entry, PRINT to print th\
+e student information, and DELETE to remove the student. If you want to quit the program, enter QUIT" <\
+< endl;
+  char playerInput[100];
+
+  cin >> playerInput;
+
+  //The "ADD" function (When the player types ADD)
+  if (strcmp(playerInput, "ADD") == 0) {
+    addCmd(vecList);
+  }
+
+  //Function for the "QUIT" Cmd
+  if (strcmp(playerInput, "QUIT") == 0) {
+    //print(vecList)
+    exit(0);
+  }
+
+  //Function for "PRINT" Command
+  if (strcmp (playerInput,"PRINT") ==0) {  
+      printCmd(vecList);
+
+   }
+
+  //Functionn for "DELETE"
+  if (strcmp(playerInput, "DELETE") ==0) {
+    deleteCmd(vecList);
+  } // cmdDelete if state ment
+
+  }  //While Loop Brackjet
+
+ }//  Main Function Bracket
+
+void addCmd(vector<structureInfo> &vecList) {
+    structureInfo students;
+    char firstName [20];
+    char lastName [20];
+    int studentID;
+    float GPA;
+
+
+    cout << "What is the students first name" <<endl;
+    cin >> firstName;
+
+    cout << "What is the Students last name" <<endl;
+    cin >> lastName;
+  cout << "What is the students id?" <<endl;
+    cin >> studentID;
+
+    cout << "What is their gpa?" <<endl;
+    cin >> GPA;
+
+    //Copying the strings that have been entered into the struct
+    strcpy (students.firstName, firstName);
+    strcpy (students.lastName, lastName);
+    students.studentId = studentID;
+    students.GPA = GPA;
+
+    //Inserting the students "structure" into the vector
+    vecList.push_back(students);
+}
+
+void printCmd(vector<structureInfo> &vecList) {
+    //Iterator is the way to loop through vectors
+    for (std::vector<structureInfo>:: iterator i= vecList.begin(); i != vecList.end(); i++) {
+
+      //To print you use ->
+      cout <<i -> firstName << " " << i -> lastName << "  " << i -> studentId<< " " << i -> GPA << endl;
+
+    }
+   
+   
+}
+
+void deleteCmd(vector <structureInfo> &vecList) {
+    int removeStudentID;
+    cout << "Which student do you want to remove?" <<endl;
+    cin >> removeStudentID;
+    int currentIndex = 0;
+
+
+    //Looping through our vector list
+    for (std::vector<structureInfo>:: iterator r = vecList.begin(); r != vecList.end(); r++) {
+      if (r -> studentId == removeStudentID) {
+
+        //Takes value of current index
+        int value = currentIndex -1;
+
+        //Erases the data inside the vector at the current index value
+        vecList.erase(vecList.begin()+currentIndex);
+
+        break;
+      }
+    }
+
+}
+
